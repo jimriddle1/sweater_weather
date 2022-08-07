ca4e4c6a375fc71de326c3ad4daa5d2c85069df2
Jim Riddle <jimriddle0@gmail.com>
2022-08-07 16:32:31 -0500

    serializer included to have request spec test pass

--- spec/api/v1/forecast_request_spec.rb
+++ spec/api/v1/forecast_request_spec.rb
@@ -1,30 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe 'Forcast API' do
-  xit 'returns the current forecast' do
+  it 'returns the current forecast' do
 
 
-    get "/api/v1/forecast?location=denver,co"
+    get "/api/v1/forecast?location=chicago,il"
     expect(response).to be_successful
 
     response_body = JSON.parse(response.body, symbolize_names: true)
     forecast = response_body[:data]
-    # binding.pry
-    # expect(items.count).to eq(3)
-    # #
-    # items.each do |item|
-    #   expect(item).to have_key(:id)
-    #   expect(item[:id]).to be_an(String)
-    #
-    #   expect(item).to have_key(:attributes)
-    #   expect(item[:attributes][:name]).to be_a(String)
-    #
-    #   expect(item).to have_key(:attributes)
-    #   expect(item[:attributes][:description]).to be_a(String)
-    #
-    #   expect(item).to have_key(:attributes)
-    #   expect(item[:attributes][:unit_price]).to be_a(Float)
-    # end
+
+    expect(forecast[:id]).to eq("null")
+    expect(forecast[:type]).to eq("forecast")
+
+    current_weather = forecast[:attributes][:current_weather]
+
+    expect(current_weather).to be_a Hash
+
+    expect(current_weather).to include :datetime, :sunrise, :sunset, :temperature, :feels_like, :humidity, :uvi, :visibility, :conditions, :icon
+    expect(current_weather[:datetime]).to be_a(String)
+    expect(current_weather[:sunrise]).to be_a(String)
+    expect(current_weather[:sunset]).to be_a(String)
+    expect(current_weather[:temperature]).to be_a(Float)
+    expect(current_weather[:feels_like]).to be_a(Float)
+    expect(current_weather[:humidity]).to be_a(Integer)
+    expect(current_weather[:uvi]).to be_a(Float)
+    expect(current_weather[:visibility]).to be_a(Integer)
+    expect(current_weather[:conditions]).to be_a(String)
+    expect(current_weather[:icon]).to be_a(String)
+
+    daily_weather = forecast[:attributes][:daily_weather]
+    expect(daily_weather).to be_an Array
+
+    first_day = daily_weather.first
+    expect(first_day).to include :datetime, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon
+    expect(first_day[:datetime]).to be_a(String)
+    expect(first_day[:sunrise]).to be_a(String)
+    expect(first_day[:sunset]).to be_a(String)
+    expect(first_day[:max_temp]).to be_a(Float)
+    expect(first_day[:min_temp]).to be_a(Float)
+    expect(first_day[:conditions]).to be_a(String)
+    expect(first_day[:icon]).to be_a(String)
+
+    hourly_weather = forecast[:attributes][:hourly_weather]
+    expect(hourly_weather).to be_an Array
+
+    first_hour = hourly_weather.first
+    expect(first_hour).to include :time, :temperature, :conditions, :icon
+    expect(first_hour[:time]).to be_a(String)
+    expect(first_hour[:temperature]).to be_a(Float)
+    expect(first_hour[:conditions]).to be_a(String)
+    expect(first_hour[:icon]).to be_a(String)
+
   end
 
 end
