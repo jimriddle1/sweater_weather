1aebf23cf19f2965fb5303867efba03fdecde108
Jim Riddle <jimriddle0@gmail.com>
2022-08-08 10:02:40 -0500

    started creating user request tests

---
+++ spec/api/v1/user_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'User API' do
+  it 'returns an api key when a user is created' do
+
+    header = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+    user_body = {  }
+
+    post "/api/v1/users, headers:header"
+    # expect(response).to be_successful
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+
+  end
+
+end
