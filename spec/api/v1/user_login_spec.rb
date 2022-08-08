21be36a5f8450242acd4177911eb0d3988e0700c
Jim Riddle <jimriddle0@gmail.com>
2022-08-08 15:34:44 -0500

    flushed out sad path testing for user login

--- spec/api/v1/user_login_spec.rb
+++ spec/api/v1/user_login_spec.rb
@@ -13,17 +13,18 @@ RSpec.describe 'User API' do
 
   end
 
-  it 'returns an api key when a user is created', :vcr do
+  it 'returns an api key when a user is logged in', :vcr do
     login_body = {
       "email": "whatever@example.com",
       "password": "password"
     }
-    post "/api/v1/sessions", headers:@header, params: JSON.generate(body)
+    post "/api/v1/sessions", headers:@header, params: JSON.generate(login_body)
 
     expect(response).to be_successful
 
     response_body = JSON.parse(response.body, symbolize_names: true)
     data = response_body[:data]
+    # binding.pry
 
     expect(data[:id]).to be_a String
     expect(data[:type]).to eq("users")
@@ -36,51 +37,22 @@ RSpec.describe 'User API' do
 
   end
 
-  xit 'returns an api key when a user is created: sad path passwords do not match', :vcr do
+  it 'returns an api key when a user is logged in: sad path incorrect password', :vcr do
 
-    header = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
-
-    body = {
+    login_body = {
       "email": "whatever@example.com",
-      "password": "password",
-      "password_confirmation": "other_password"
-      }
-    post "/api/v1/users", headers:header, params: JSON.generate(body)
+      "password": "password_2"
+    }
+
+    post "/api/v1/sessions", headers:@header, params: JSON.generate(login_body)
 
     expect(response).to_not be_successful
 
     response_body = JSON.parse(response.body, symbolize_names: true)
     data = response_body[:data]
-    expect(data[:errors]).to eq("doesn't match Password")
+    expect(data[:errors]).to eq("Invalid Credentials")
 
   end
 
-  xit 'returns an api key when a user is created: sad path user already exists', :vcr do
-
-    header = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
-
-    body = {
-      "email": "whatever@example.com",
-      "password": "password",
-      "password_confirmation": "password"
-      }
-    post "/api/v1/users", headers:header, params: JSON.generate(body)
-
-    header = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
-
-    body = {
-      "email": "whatever@example.com",
-      "password": "password",
-      "password_confirmation": "password"
-      }
-    post "/api/v1/users", headers:header, params: JSON.generate(body)
-
-    expect(response).to_not be_successful
-
-    response_body = JSON.parse(response.body, symbolize_names: true)
-    data = response_body[:data]
-    expect(data[:errors]).to eq("has already been taken")
-
-  end
 
 end
