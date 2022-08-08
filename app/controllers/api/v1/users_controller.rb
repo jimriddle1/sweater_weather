ad5fb91a0d7f0e3f2f159f5396c912a5044436b9
Jim Riddle <jimriddle0@gmail.com>
2022-08-08 14:52:03 -0500

    included create method, will need to have sad path

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::UsersController < ApplicationController
 
-  # def create
-  #   binding.pry
-  # end
+  def create
+    user = User.create(email: params[:email],
+                  password: params[:password],
+                  password_confirmation: params[:password_confirmation],
+                  api_key: SecureRandom.uuid)
+    render json: UserSerializer.create_user(user)
+  end
+
 
 end
