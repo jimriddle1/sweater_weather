2d18ffc2b32e03231888e659e58e86224d24c59f
Jim Riddle <jimriddle0@gmail.com>
2022-08-08 18:56:16 -0500

    modified facades to handle road trips

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -6,6 +6,21 @@ class ForecastFacade
     forecast_data = ForecastService.get_forecast(lat, lon)
   end
 
+  def self.get_future_forecast(lat, lon, duration)
+    if duration.nil?
+      return nil
+    else
+      forecast_data = ForecastService.get_forecast(lat, lon)
+      hours = duration[0..1].to_i
+      if hours <= 48
+        HourlyWeather.new(forecast_data[:hourly][hours])
+      else
+        days = (hours/24).to_i
+        DailyWeather.new(forecast_data[:daily][days])
+      end
+    end
+  end
+
   def self.current_weather(forecast_data)
     current_weather_data = forecast_data[:current]
     CurrentWeather.new(current_weather_data)
--- app/facades/geocode_facade.rb
+++ app/facades/geocode_facade.rb
@@ -7,4 +7,9 @@ class GeocodeFacade
     Geocode.new(geocode_data)
   end
 
+  def self.get_route(origin, destination)
+    route_data = GeocodeService.get_route(origin, destination)
+    RoadTrip.new(route_data)
+  end
+
 end
