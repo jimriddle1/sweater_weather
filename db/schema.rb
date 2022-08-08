9503811a2eb9686a27a596c8c892a5cdc7bd4557
Jim Riddle <jimriddle0@gmail.com>
2022-08-08 14:51:42 -0500

    reconfigured user table to have correct fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_08_143311) do
+ActiveRecord::Schema.define(version: 2022_08_08_192848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password_digest"
     t.string "api_key"
+    t.string "password_digest"
   end
 
 end
