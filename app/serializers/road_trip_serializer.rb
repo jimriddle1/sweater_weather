e927d9c5277f649dae1c08a27d185a5303b76376
Jim Riddle <jimriddle0@gmail.com>
2022-08-08 18:59:02 -0500

    road trip serializer

---
+++ app/serializers/road_trip_serializer.rb
@@ -0,0 +1,36 @@
+class RoadTripSerializer
+
+  def self.setup_good_road_trip(info)
+    # binding.pry
+    {
+      "data": {
+        "id": "null",
+        "type": "road_trip",
+        "attributes": {
+          "start_city": info[:start_city],
+          "end_city": info[:end_city],
+          "travel_time": info[:travel_time],
+          "weather_at_eta": {
+            "temperature": info[:temperature].round(1),
+            "conditions": info[:conditions]
+          }
+        }
+      }
+    }
+  end
+
+  def self.setup_impossible_road_trip(info)
+    {
+      "data": {
+        "id": "null",
+        "type": "road_trip",
+        "attributes": {
+          "start_city": info[:start_city],
+          "end_city": info[:end_city],
+          "travel_time": info[:travel_time],
+          "weather_at_eta": {},
+        }
+      }
+    }
+  end
+end
